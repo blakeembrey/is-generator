306ed041cd0c361f1437b27984eaa98e868036e1
tunnckoCore <mameto_100@mail.bg>
2015-05-26 00:57:17 +0300

    update `is-generator-function`

--- is-generator.js
+++ is-generator.js
@@ -4,19 +4,25 @@
  * @param  {Object}  obj
  * @return {Boolean}
  */
-exports = module.exports = function (obj) {
-  return obj &&
-    typeof obj.next === 'function' &&
-    typeof obj.throw === 'function';
-};
+function isGenerator (obj) {
+  return obj && typeof obj.next === 'function' && typeof obj.throw === 'function'
+}
 
 /**
  * Check whether a function is generator.
  *
  * @param  {Function} fn
  * @return {Boolean}
+ * @api public
  */
-exports.fn = function (fn) {
-  return typeof fn === 'function' &&
-    fn.constructor.name === 'GeneratorFunction';
-};
+function isGeneratorFunction (obj) {
+  if (!obj) return false
+  var co = obj.constructor
+  if (!co) return false
+  if (typeof obj !== 'function') return false
+  if (co.name === 'GeneratorFunction' || co.displayName === 'GeneratorFunction') return true
+  return isGenerator(co.prototype)
+}
+
+exports = module.exports = isGenerator
+exports.fn = isGeneratorFunction
